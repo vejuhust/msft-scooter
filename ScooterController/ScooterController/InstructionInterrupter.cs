33bcef4c615f703616d82e3e223834b7c404a640
Wei Ye <vejuhust@gmail.com>
2015-07-14 17:02:43 +0800

    Load script by lines

--- ScooterController/ScooterController/InstructionInterrupter.cs
+++ ScooterController/ScooterController/InstructionInterrupter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,22 @@ namespace ScooterController
 {
     class InstructionInterrupter
     {
+        private readonly List<string> instructionRawLines;
+
         public InstructionInterrupter(string filename = "instruction.txt")
         {
-            
+            using (var file = new StreamReader(filename))
+            {
+                string line;
+
+                var lines = new List<string>();
+                while ((line = file.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+
+                this.instructionRawLines = lines;
+            }
         }
     }
 }
