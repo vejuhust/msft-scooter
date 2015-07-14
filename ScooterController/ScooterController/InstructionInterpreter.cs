366f2b3d54b30743fcfa5278dc57a5075399c656
Wei Ye <vejuhust@gmail.com>
2015-07-14 18:57:26 +0800

    Define NullaryOperator and UnaryOperator mappings

--- ScooterController/ScooterController/InstructionInterpreter.cs
+++ ScooterController/ScooterController/InstructionInterpreter.cs
@@ -7,10 +7,24 @@ namespace ScooterController
 {
     class InstructionInterpreter
     {
-
         private readonly List<string> instructionRawLines;
 
         private int counterRawLine = 0;
+
+        private static Dictionary<HardwareOperator, string> NullaryOperatorMapping = new Dictionary<HardwareOperator, string>()
+        {
+            { HardwareOperator.PowerOn, "on" },     
+            { HardwareOperator.PowerOff, "off" },     
+        };
+
+        private static Dictionary<HardwareOperator, string> UnaryOperatorMapping = new Dictionary<HardwareOperator, string>()
+        {
+            { HardwareOperator.SetSpeed, "sp" },     
+            { HardwareOperator.MoveForward, "fd" },     
+            { HardwareOperator.MoveBack, "bk" },     
+            { HardwareOperator.TurnLeft, "lt" },     
+            { HardwareOperator.TurnRight, "rt" },     
+        }; 
 
         public InstructionInterpreter(string filename = "instruction.txt")
         {
@@ -55,7 +69,7 @@ namespace ScooterController
         {
             var commentIndex = rawLine.IndexOf(HardwareInstruction.CommentSymbol, StringComparison.InvariantCultureIgnoreCase);
             var line = commentIndex >= 0 ? rawLine.Substring(0, commentIndex) : rawLine;
-            line = line.Trim();
+            line = line.Trim().ToLowerInvariant();
             return !string.IsNullOrWhiteSpace(line) ? line : null;
         }
     }
