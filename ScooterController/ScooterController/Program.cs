0a3fa2ea04da2c9cce712af7d4cdf5f89672495b
Wei Ye <vejuhust@gmail.com>
2015-07-15 01:48:09 +0800

    Execute instructions from parsed SCL scripts

--- ScooterController/ScooterController/Program.cs
+++ ScooterController/ScooterController/Program.cs
@@ -6,26 +6,19 @@ namespace ScooterController
     {
         static void Main(string[] args)
         {
-            //var controller = new HardwareController();
-            //controller.ExecuteInstruction(new HardwareInstruction(HardwareOperator.MoveForward, 1));
-            //controller.ExecuteInstruction(new HardwareInstruction(HardwareOperator.MoveBack, 1));
-            //controller.ExecuteInstruction(new HardwareInstruction(HardwareOperator.TurnLeft, 1));
-            //controller.ExecuteInstruction(new HardwareInstruction(HardwareOperator.TurnRight, 1));
-            //controller.ExecuteInstruction(new HardwareInstruction(HardwareOperator.SetSpeed, 3));
-                                                                                                
-
+            var controller = new HardwareController();
             var parser = new InstructionInterpreter();
 
             HardwareInstruction instruction;
             while ((instruction = parser.GetNextInstruction()) != null)
             {
-                Console.WriteLine("{0} {1}", instruction.Operator, instruction.HasOperand ? instruction.Operand.ToString() : string.Empty);
-
                 if (instruction.Operator == HardwareOperator.NoOp || instruction.Operator == HardwareOperator.Exit)
                 {
                     break;
                 }
 
+                Console.WriteLine("\n[{0} {1}]", instruction.Operator, instruction.HasOperand ? instruction.Operand.ToString() : string.Empty);
+                controller.ExecuteInstruction(instruction);
             }
         }
     }
